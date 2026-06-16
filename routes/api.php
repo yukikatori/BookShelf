30752fd066fd742d81767faab69cfa924d0a8473
Katori <yuki.katori1@outlook.jp>
2026-06-16 23:09:38 +0900

    feat: Sanctum認証 #29

--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,23 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\V1\AuthController;
 use App\Http\Controllers\Api\V1\BookController;
 
 Route::prefix('v1')->group(function () {
-    Route::apiResource('books', BookController::class);
+    // 認証
+    Route::post('/login', [AuthController::class, 'login']);
+
+    // 公開API
+    Route::get('/books', [BookController::class, 'index']);
+    Route::get('/books/{book}', [BookController::class, 'show']);
+
+    // 認証が必要なAPI
+    Route::middleware('auth:sanctum', 'throttle:api')->group(function () {
+        Route::post('/books', [BookController::class, 'store']);
+        Route::put('/books/{book}', [BookController::class, 'update']);
+        Route::delete('/books/{book}', [BookController::class, 'destroy']);
+    });
 });
 
 
